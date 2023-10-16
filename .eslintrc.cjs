cd31b6ea6f74757d50a7d290f02720b3c3838285
viadsten <stanulevich.vg@yandex.ru>
2023-10-16 12:55:06 +0300

    fix image lint errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,8 @@ module.exports = {
         'es2021': true
     },
     'globals': {
-      'Fragment': 'readonly'
+      'Fragment': 'readonly',
+      'ImageMetadata': 'readonly'
     },
     'overrides': [
         {
